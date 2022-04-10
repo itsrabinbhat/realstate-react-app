e11229e04d1371770e4c9b36722c1fd13ac14447
rabinbhat <bhatravi984@gmail.com>
2022-04-10 19:23:06 +0545

    homepage completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import Contact from "./components/Contact";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Home />
+      <Contact />
     </div>
   );
 };
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import FormatQuoteIcon from "@mui/icons-material/FormatQuote";
 import "../assets/css/Home.css";
 
 const Home = () => {
@@ -42,7 +43,74 @@ const Home = () => {
           ))}
         </div>
       </div>
-      <div className="query-container"></div>
+      <div className="query-container">
+        <div className="img-container">
+          <div className="img">{/*image goes here...*/}</div>
+        </div>
+        <div className="desc-container">
+          <p className="title">Why choose us?</p>
+          <p className="desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Consequatur, quasi eveniet! Autem adipisci aut ea maxime laudantium!
+          </p>
+
+          <p className="desc">
+            Quasi autem natus tenetur beatae libero est repellat facere. Illum,
+            eaque deleniti! Lorem ipsum dolor sit amet consectetur, adipisicing
+            elit. Excepturi cum sunt corporis aperiam sint! Deserunt similique
+            aliquam iure! Totam temporibus tenetur cupiditate iste asperiores
+            perferendis tempore vel quod quos laboriosam.
+          </p>
+          <button className="btn-primary">
+            Learn More <ArrowForwardIcon style={{ marginLeft: "8px" }} />
+          </button>
+        </div>
+      </div>
+
+      <div className="review-container">
+        <div className="head">
+          <div className="title">What customers say about us?</div>
+          <button className="btn-primary">View More...</button>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <FormatQuoteIcon
+              sx={{ transform: "rotate(180deg)" }}
+              className="quote"
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque
+              corporis repellat dicta culpa amet non.
+            </p>
+            <FormatQuoteIcon className="quote" />
+            <p className="author">Iron man</p>
+          </div>
+          <div className="card">
+            <FormatQuoteIcon
+              sx={{ transform: "rotate(180deg)" }}
+              className="quote"
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque
+              corporis repellat dicta culpa amet non.
+            </p>
+            <FormatQuoteIcon className="quote" />
+            <p className="author">Thor</p>
+          </div>
+          <div className="card">
+            <FormatQuoteIcon
+              sx={{ transform: "rotate(180deg)" }}
+              className="quote"
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque
+              corporis repellat dicta culpa amet non.
+            </p>
+            <FormatQuoteIcon className="quote" />
+            <p className="author">The Hulk</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
