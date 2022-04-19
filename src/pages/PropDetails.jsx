d841b786dd31c179c98464cd8eb7c50493228988
rabinbhat <bhatravi984@gmail.com>
2022-04-19 20:09:21 +0545

    Propery detail page completed

--- src/pages/PropDetails.jsx
+++ src/pages/PropDetails.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { useGetPropDetailsQuery } from "../services/api";
 import { Carousel } from "react-responsive-carousel";
+import { FaMobile, FaPhone, FaWhatsappSquare } from "react-icons/fa";
 import millify from "millify";
 import HTMLReactParser from "html-react-parser";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
@@ -31,8 +32,15 @@ const PropDetails = () => {
           <div className="left">
             <div className="highlights">
               <div className="highlight">
-                <p className="title">Price</p>
-                <p className="value">{millify(data?.price)}</p>
+                <p className="title">Price(AED)</p>
+                <p className="value">
+                  {millify(data?.price)}
+                  {data?.purpose == "for-rent" && "/Monthly"}
+                </p>
+              </div>
+              <div className="highlight">
+                <p className="title">Product Type</p>
+                <p className="value">{data?.product}</p>
               </div>
               <div className="highlight">
                 <p className="title">Area Coverd</p>
@@ -52,6 +60,44 @@ const PropDetails = () => {
                 <li key={idx}>{item.amenities[0]?.text}</li>
               ))}
             </ul>
+          </div>
+        </div>
+
+        <h3 className="seller-info-title">Seller</h3>
+        <div className="seller-info">
+          <div className="left">
+            <p className="title">{data?.agency?.name}</p>
+            <img src={data?.agency?.logo?.url} alt="agency-logo" />
+            <p className="desc">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis,
+              saepe soluta ab non vero molestias quasi voluptatum dolorem iure
+              quas deserunt nulla, rem molestiae sed. Nemo, facilis recusandae,
+              aliquid consequatur cumque officia veniam repellat ea expedita
+              quasi laboriosam soluta quod obcaecati eaque adipisci repellendus
+              earum? Itaque nemo ipsa dolorem earum.
+            </p>
+          </div>
+          <div className="right">
+            <p className="title">Contact Info</p>
+            <p className="name">{data?.contactName}</p>
+            <div className="contact">
+              <FaMobile className="icon" />
+              {data?.phoneNumber?.mobile
+                ? data?.phoneNumber?.mobile
+                : "Not Availabe."}
+            </div>
+            <div className="contact">
+              <FaPhone className="icon" />
+              {data?.phoneNumber?.phone
+                ? data?.phoneNumber?.phone
+                : "Not Availabe."}
+            </div>
+            <div className="contact">
+              <FaWhatsappSquare className="icon" />
+              {data?.phoneNumber?.whatsapp
+                ? data?.phoneNumber?.whatsapp
+                : "Not Availabe."}
+            </div>
           </div>
         </div>
       </div>
